02e71aaf1a8373558615cab5c6dc825c94944aab
David House <davidahouse@gmail.com>
2021-02-13 10:23:29 -0500

    :sparkles: lines of code details screen

---
+++ controllers/repositories/linesOfCodeDetails.js
@@ -0,0 +1,120 @@
+const prettyMilliseconds = require("pretty-ms");
+const randomColor = require("randomcolor");
+
+/**
+ * path this handler will serve
+ */
+function path() {
+  return "/repositories/linesOfCodeDetails";
+}
+
+/**
+ * handle index
+ * @param {*} req
+ * @param {*} res
+ * @param {*} dependencies
+ */
+async function handle(req, res, dependencies, owners) {
+  const owner = req.query.owner;
+  const repository = req.query.repository;
+
+  const repositoryDetails = await dependencies.db.repositories.fetchRepository(
+    owner,
+    repository
+  );
+  let defaultBranch = "";
+  if (repositoryDetails == null || repositoryDetails.rows.length == 0) {
+    defaultBranch = dependencies.serverConfig.defaultBranch;
+  } else {
+    defaultBranch = repositoryDetails.rows[0].default_branch;
+  }
+
+  const linesOfCode = await fetchLinesOfCode(
+    owner,
+    repository,
+    defaultBranch,
+    dependencies
+  );
+
+  console.dir(linesOfCode.languages);
+
+  const sortedLanguages = linesOfCode.languages.sort(function (a, b) {
+    const aloc = parseInt(a.code_lines);
+    const bloc = parseInt(b.code_lines);
+    if (aloc < bloc) {
+      return 1;
+    } else if (aloc > bloc) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  res.render(dependencies.viewsPath + "repositories/linesOfCodeDetails", {
+    owners: owners,
+    isAdmin: req.validAdminSession,
+    owner: owner,
+    repository: repository,
+    branch: defaultBranch,
+    linesOfCode: linesOfCode.linesOfCode,
+    totalLinesOfCode: linesOfCode.totalLinesOfCode,
+    totalFiles: linesOfCode.totalFiles,
+    languages: sortedLanguages,
+    prettyMilliseconds: (ms) => (ms != null ? prettyMilliseconds(ms) : ""),
+  });
+}
+
+async function fetchLinesOfCode(owner, repository, branch, dependencies) {
+  let linesOfCode = null;
+  const linesOfCodeData = await dependencies.db.linesofcode.fetchLatestLinesOfCode(
+    owner,
+    repository,
+    branch
+  );
+
+  let totalLinesOfCode = 0;
+  let totalFiles = 0;
+
+  if (linesOfCodeData != null) {
+    const total = [];
+    const labels = [];
+    const colors = [];
+
+    for (let index = 0; index < linesOfCodeData.languages.length; index++) {
+      totalLinesOfCode += linesOfCodeData.languages[index].code_lines;
+      totalFiles += linesOfCodeData.languages[index].number_of_files;
+    }
+
+    for (let index = 0; index < linesOfCodeData.languages.length; index++) {
+      const pct = Math.round(
+        (linesOfCodeData.languages[index].code_lines / totalLinesOfCode) * 100
+      );
+      labels.push(
+        linesOfCodeData.languages[index].language + " (" + pct.toString() + "%)"
+      );
+      total.push(pct);
+      colors.push(randomColor());
+    }
+
+    linesOfCode = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Language",
+          data: total,
+          backgroundColor: colors,
+        },
+      ],
+    };
+  }
+
+  return {
+    linesOfCode: linesOfCode,
+    totalLinesOfCode: totalLinesOfCode,
+    totalFiles: totalFiles,
+    languages: linesOfCodeData.languages,
+  };
+}
+
+module.exports.path = path;
+module.exports.handle = handle;
