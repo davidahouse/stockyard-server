f709d03bb0c84515118ca934ec352b686a61a0e9
David House <davidahouse@gmail.com>
2021-02-12 19:21:02 -0500

    :sparkles: image capture tables

---
+++ api/upload/imagecapture.js
@@ -0,0 +1,82 @@
+"use strict";
+
+/**
+ * The url path this handler will serve
+ */
+function path() {
+  return "/api/upload/imagecapture";
+}
+
+/**
+ * The http method this handler will serve
+ */
+function method() {
+  return "post";
+}
+
+/**
+ * handle activeBuilds
+ * @param {*} req
+ * @param {*} res
+ * @param {*} dependencies
+ */
+async function handle(req, res, dependencies) {
+  const owner = req.query.owner;
+  const repository = req.query.repository;
+  const branch = req.query.branch;
+  const pullRequest = req.query.pull_request;
+
+  if (owner == null || repository == null || branch == null) {
+    res.send({ status: "missing owner, repo or branch" });
+    return;
+  }
+
+  const contents = req.body;
+
+  await dependencies.db.repositories.storeRepository(
+    owner,
+    repository,
+    dependencies.serverConfig.defaultBranch
+  );
+  await dependencies.db.branches.storeBranch(
+    owner,
+    repository,
+    branch,
+    pullRequest
+  );
+  await dependencies.db.branches.updateBranchActivity(
+    owner,
+    repository,
+    branch
+  );
+
+  await dependencies.db.imagecapture.storeImageCapture(
+    owner,
+    repository,
+    branch,
+    contents.images,
+  );
+
+  res.send({ status: "ok" });
+}
+
+/**
+ * The OpenAPI docs
+ */
+function docs() {
+  return {
+    get: {
+      summary: "imagecapture",
+      responses: {
+        200: {
+          description: "",
+        },
+      },
+    },
+  };
+}
+
+module.exports.path = path;
+module.exports.method = method;
+module.exports.handle = handle;
+module.exports.docs = docs;
