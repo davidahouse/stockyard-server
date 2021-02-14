f66ffb50fdae6d39d4e124272233affcee4ef66f
David House <davidahouse@gmail.com>
2021-02-14 09:58:33 -0500

    :sparkles: Added code coverage details screen

---
+++ controllers/repositories/codeCoverageDetails.js
@@ -0,0 +1,108 @@
+const prettyMilliseconds = require("pretty-ms");
+const randomColor = require("randomcolor");
+
+/**
+ * path this handler will serve
+ */
+function path() {
+  return "/repositories/codeCoverageDetails";
+}
+
+/**
+ * handle index
+ * @param {*} req
+ * @param {*} res
+ * @param {*} dependencies
+ */
+async function handle(req, res, dependencies, owners) {
+  const owner = req.query.owner;
+  const repository = req.query.repository;
+
+  const repositoryDetails = await dependencies.db.repositories.fetchRepository(
+    owner,
+    repository
+  );
+  let defaultBranch = "";
+  if (repositoryDetails == null || repositoryDetails.rows.length == 0) {
+    defaultBranch = dependencies.serverConfig.defaultBranch;
+  } else {
+    defaultBranch = repositoryDetails.rows[0].default_branch;
+  }
+
+  const codeCoverage = await fetchCodeCoverage(
+    owner,
+    repository,
+    defaultBranch,
+    dependencies
+  );
+
+  const coverageDetails = await dependencies.db.codecoverage.fetchCodeCoverageFiles(
+    codeCoverage.codeCoverageID
+  );
+
+  res.render(dependencies.viewsPath + "repositories/codeCoverageDetails", {
+    owners: owners,
+    isAdmin: req.validAdminSession,
+    owner: owner,
+    repository: repository,
+    branch: defaultBranch,
+    coveragePct: codeCoverage.coveragePct,
+    coverageFiles: codeCoverage.files,
+    noCoveragePct: codeCoverage.noCoveragePct,
+    noCoverageFiles: codeCoverage.noCoverageFiles,
+    goodCoveragePct: codeCoverage.goodCoveragePct,
+    goodCoverageFiles: codeCoverage.goodCoverageFiles,
+    coverageDetails: coverageDetails,
+    prettyMilliseconds: (ms) => (ms != null ? prettyMilliseconds(ms) : ""),
+  });
+}
+
+async function fetchCodeCoverage(owner, repository, branch, dependencies) {
+  const latest = await dependencies.db.codecoverage.fetchLatestCodeCoverageSummary(
+    owner,
+    repository,
+    branch
+  );
+  if (latest == null) {
+    return {
+      coveragePct: 0.0,
+      files: 0,
+      noCoveragePct: 0.0,
+      noCoverageFiles: 0,
+      goodCoveragePct: 0.0,
+      goodCoverageFiles: 0,
+    };
+  } else {
+    let totalFiles = 0;
+    let noCoverageFiles = 0;
+    let goodCoverageFiles = 0;
+    latest.summary.forEach(function (file) {
+      totalFiles += 1;
+      if (file.line_coverage <= 0.0) {
+        noCoverageFiles += 1;
+      } else if (file.line_coverage >= 0.9) {
+        goodCoverageFiles += 1;
+      }
+    });
+    let noCoveragePct = 0;
+    if (totalFiles > 0) {
+      noCoveragePct = noCoverageFiles / totalFiles;
+    }
+    let goodCoveragePct = 0;
+    if (totalFiles > 0) {
+      goodCoveragePct = goodCoverageFiles / totalFiles;
+    }
+    return {
+      codeCoverageID: latest.codeCoverageID,
+      coveragePct: Math.round(latest.lineCoverage * 100),
+      files: totalFiles,
+      noCoveragePct: Math.round(noCoveragePct * 100),
+      noCoverageFiles: noCoverageFiles,
+      goodCoveragePct: Math.round(goodCoveragePct * 100),
+      goodCoverageFiles: goodCoverageFiles,
+    };
+  }
+}
+
+module.exports.path = path;
+module.exports.handle = handle;
